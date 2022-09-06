20360d16d2801800f30a76852dfede18d2d1df33
Mahmoud Ashraf Algammal <engmahmoudalgamal@gmail.com>
2022-09-06 17:00:48 +0200

    imp

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const express = require("express");
+
+var cors = require('cors');
+
+const app = express();
+const port = 8000;
+let userBalance = 0;
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello Tap Payment Server!");
+});
+app.get("/api/balance", (req, res) => {
+  res.status(200).send({
+    balance: userBalance
+  });
+});
+app.post("/api/balance", (req, res) => {
+  const {
+    amount
+  } = req.body;
+  if (typeof amount !== "number" && amount > 0) return res.status(400).send({
+    error: "Invalid amount!"
+  });
+  userBalance += amount;
+  res.status(200).send();
+});
+app.listen(port, () => {
+  console.log(`server app listening at http://localhost:${port}`);
+});
